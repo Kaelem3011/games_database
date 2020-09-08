32ea9a9df1e233a2ccbdf56757e73de9a83c6b03
Kaelem <51727185+Kaelem3011@users.noreply.github.com>
2020-09-09 10:55:58 +1200

    finished star rating

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
             
             <p>
                 This database includes some of the most popular apps
-                available for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can 
+                available for mobile devices. 
+            <br />  
+                <h2>Acknowledgements</h2>
+                The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can 
                 be found at the preceding link.
+            <br />
+                The code for the <a href="https://codepen.io/Bluetidepro/pen/GkpEa">star percentage code</a> can be found at this link
             </p>
             
         </div> <!-- / main -->
